8818f5dd40abbddaa669e3ece94cdf00d0dc7f68
Avinash Singh <avinash4507@gmail.com>
2022-02-11 18:11:17 -0800

    updated string reversal

--- stringReversal.js
+++ stringReversal.js
@@ -1,4 +1,4 @@
-const string = 'Hi My Name Is Avinash';
+const string = ' Hi My Name Is Avinash ';
 reverseString = (str) => {
   if (!str || str.length < 2 || typeof str !== 'string' ) {
     return "that's bad input";
@@ -6,4 +6,12 @@ reverseString = (str) => {
   return str.split('').reverse().join('');
 }
 const reverseSt = (str) => [...str].reverse().join('');
-console.log(reverseSt(string));
+// console.log(reverseSt(string));
+
+reverseStringRecursively = (str) => {
+  if (str.length < 2) {
+    return str;
+  }
+  return reverseStringRecursively(str.substring(1)) +  str.charAt(0);
+}
+console.log(reverseStringRecursively(string));
